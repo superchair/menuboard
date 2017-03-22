7d1c3dd63c17abdab7db4e722802defebf314104
Alex Brown <alex.edward.brown@gmail.com>
2017-03-22 09:04:49 -0400

    Adding prices, code cleanup

--- REST.js
+++ REST.js
@@ -12,18 +12,76 @@ function REST_ROUTER(router,connection,md5) {
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get('/', function(req, res) {
-        res.json(
-            {
-                'Error': false,
-                'Message': 'Status: OK'
+        res.json({
+            'Error': false,
+            'Message': 'Status: OK'
+        });
+    });
+
+    router.get('/products', function(req, res) {
+        console.log('Fetching all products...');
+        var query = 'SELECT * FROM ??';
+        var table = [
+            'products'
+        ];
+
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if(err) {
+                var errStr = 'Error getting rows: ' + err;
+                console.log(errStr);
+                res.json({
+                    'Error': true,
+                    'Message': errStr
+                });
+            } else {
+                var count = rows ? rows.length : 0;
+                console.log('Got ' + count + ' rows');
+                res.json({
+                    'Error': false,
+                    'Message': 'OK',
+                    'count': count,
+                    'products': rows
+                });
             }
-        );
+        });
     });
 
-    router.post('/products', function(req, res) {
+    router.get('/products/:product_id', function(req, res) {
+        console.log('Fetching product with id: ' + req.params.product_id);
+        var query = 'SELECT * FROM ?? WHERE ??=?';
+        var table = [
+            'products',
+            'id',
+            req.params.product_id
+        ];
+
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if(err) {
+                var errStr = 'Error getting rows: ' + err;
+                console.log(errStr);
+                res.json({
+                    'Error': true,
+                    'Message': errStr
+                });
+            } else if(!rows || rows.length == 0) {
+                res.json({
+                    'Error': true,
+                    'Message': 'Product id ' + req.params.product_id + ' does not exist'
+                });
+            } else {
+                res.json({
+                    'Error': false,
+                    'Message': 'OK',
+                    'products': rows[0]
+                });
+            }
+        });
     });
 
-    router.post('/product', function(req, res) {
+    router.put('/products', function(req, res) {
+        console.log('creating new product');
         function onFormParsed(res, name, img) {
             if(name) {
                 var query = 'INSERT INTO ??(??,??) VALUES (?,?)';
@@ -36,34 +94,31 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
                 ];
                 query = mysql.format(query, table);
                 connection.query(query,function(err, rows){
-                    if(err) {
-                        res.json(
-                            {
-                                'Error': true,
-                                'Message': 'Error executing MySQL query'
-                            }
-                        );
+                    if(err || !rows) {
+                        var errStr = 'Error executing query';
+                        console.log(errStr);
+                        res.json({
+                            'Error': true,
+                            'Message': errStr
+                        });
                     } else {
-                        res.json(
-                            {
-                                'Error': false,
-                                'Message': 'OK',
-                                'product': {
-                                    id: rows.insertId,
-                                    name: name,
-                                    img: imgPath + img
-                                }
+                        console.log('Product created with id: ' + rows.insertId);
+                        res.json({
+                            'Error': false,
+                            'Message': 'OK',
+                            'product': {
+                                id: rows.insertId,
+                                name: name,
+                                img: img
                             }
-                        );
+                        });
                     }
                 });
             } else {
-                res.json(
-                    {
-                        'Error': true,
-                        'Message': 'A product \'name\' must be included'
-                    }
-                );
+                res.json({
+                    'Error': true,
+                    'Message': 'A product \'name\' must be included'
+                });
             }
         }
 
@@ -89,32 +144,40 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
 
             req.pipe(req.busboy);
         } else {
-            res.json(
-                {
-                    'Error': true,
-                    'Message': 'Could not parse form data'
-                }
-            );
+            var errStr = 'Could not parse form data';
+            console.log(errStr);
+            res.json({
+                'Error': true,
+                'Message': errStr
+            });
         }
 
     });
 
-    router.delete('/product', function(req, res) {
+    router.delete('/products/:product_id', function(req, res) {
+        console.log('Deleting product with id: ' + req.params.product_id);
         var getQuery = 'SELECT * FROM ?? WHERE ??=?';
         var getTable = [
             'products',
-            'name',
-            req.body.name
+            'id',
+            req.params.product_id
         ];
         getQuery = mysql.format(getQuery, getTable);
         connection.query(getQuery,function(err, rows){
             if(err) {
-                res.json(
-                    {
-                        'Error': true,
-                        'Message': 'Error executing MySQL query'
-                    }
-                );
+                var errStr = 'Error executing query';
+                console.log(errStr);
+                res.json({
+                    'Error': true,
+                    'Message': errStr
+                });
+            } else if(!rows || rows.length == 0) {
+                var errStr = 'Product id ' + req.params.product_id + ' does not exist';
+                console.log(errStr);
+                res.json({
+                    'Error': true,
+                    'Message': errStr
+                });
             } else {
                 // this should only return 1 row
                 for(var i = 0; i < rows.length; i++) {
@@ -134,35 +197,120 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
                     var delQuery = 'DELETE FROM ?? WHERE ??=?';
                     var delTable = [
                         'products',
-                        'name',
-                        req.body.name
+                        'id',
+                        req.params.product_id
                     ];
                     delQuery = mysql.format(delQuery, delTable);
                     connection.query(delQuery,function(err, rows){
                         if(err) {
-                            res.json(
-                                {
-                                    'Error': true,
-                                    'Message': 'Error executing MySQL query'
+                            var errStr = 'Error executing query';
+                            console.log(errStr);
+                            res.json({
+                                'Error': true,
+                                'Message': errStr
+                            });
+                        } else {
+                            res.json({
+                                'Error': false,
+                                'Message': 'OK',
+                                'product': {
+                                    id: row.id,
+                                    name: row.name,
+                                    img: row.img
                                 }
-                            );
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    });
+
+    router.get('/products/:product_id/prices', function(req, res) {
+        console.log('Getting prices for product id: ' + req.params.product_id);
+        var query = 'SELECT * FROM ?? WHERE ??=?';
+        var table = [
+            'prices',
+            'product_id',
+            req.params.product_id
+        ];
+
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if(err) {
+                var errStr = 'Error executing query';
+                console.log(errStr);
+                res.json({
+                    'Error': true,
+                    'Message': errStr
+                });
+            } else {
+                res.json({
+                    'Error': false,
+                    'Message': 'OK',
+                    'count': rows ? rows.length : 0,
+                    'products': rows
+                });
+            }
+        });
+    });
+
+    router.put('/products/:product_id/prices', function(req, res) {
+        if(req.busboy) {
+            var price;
+
+            req.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+                if(fieldname == 'price') {
+                    price= val;
+                }
+            });
+
+            req.busboy.on('finish', function() {
+                if(!price) {
+                    res.json({
+                        'Error': true,
+                        'Message': 'Need to include price value'
+                    });
+                } else {
+                    var query = 'INSERT INTO ??(??,??) VALUES (?,?)';
+                    var table = [
+                        'prices',
+                        'productId',
+                        'price',
+                        req.params.product_id,
+                        price
+                    ];
+
+                    query = mysql.format(query, table);
+                    connection.query(query, function(err, rows) {
+                        if(err || !rows) {
+                            console.log('Error when inserting into DB', err);
                         } else {
                             res.json(
                                 {
                                     'Error': false,
                                     'Message': 'OK',
-                                    'product': {
-                                        id: row.id,
-                                        name: row.name,
-                                        img: row.img
+                                    'price': {
+                                        'id': rows.insertId,
+                                        'productId': req.params.product_id,
+                                        'price': price
                                     }
                                 }
                             );
                         }
                     });
                 }
-            }
-        });
+            });
+
+            req.pipe(req.busboy);
+        } else {
+            res.json(
+                {
+                    'Error': true,
+                    'Message': 'Could not parse form data'
+                }
+            );
+        }
     });
 }
 
